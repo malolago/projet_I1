1240ce2d913bafc5862526683c96097cfac9cc98
Malo <lagorcemalo@gmail.com>
2025-11-17 21:32:03 +0100

    Ajout des tests sur les placements des navires

--- test_bataille_navale.py
+++ test_bataille_navale.py
@@ -33,4 +33,60 @@ class TestInitialisation:
         assert jeu.navires[0] == ('Porte-avions', 5)
         assert jeu.navires[1] == ('Croiseur', 4)
         assert jeu.navires[4] == ('Patrouilleur', 2)
+
+
+class TestPlacementNavires:
+    """Tests pour le placement des navires"""
+    
+    def test_placement_horizontal_valide(self):
+        jeu = BatailleNavale()
+        grille = [[' ' for _ in range(10)] for _ in range(10)]
         
+        # Simuler un placement horizontal en position (0,0) de longueur 3
+        positions = [(0, 0), (0, 1), (0, 2)]
+        for x, y in positions:
+            grille[x][y] = 'N'
+        
+        # Vérifier que le navire est bien placé
+        assert grille[0][0] == 'N'
+        assert grille[0][1] == 'N'
+        assert grille[0][2] == 'N'
+        assert grille[0][3] == ' '
+    
+    def test_placement_vertical_valide(self):
+        jeu = BatailleNavale()
+        grille = [[' ' for _ in range(10)] for _ in range(10)]
+        
+        # Simuler un placement vertical en position (0,0) de longueur 3
+        positions = [(0, 0), (1, 0), (2, 0)]
+        for x, y in positions:
+            grille[x][y] = 'N'
+        
+        # Vérifier que le navire est bien placé
+        assert grille[0][0] == 'N'
+        assert grille[1][0] == 'N'
+        assert grille[2][0] == 'N'
+        assert grille[3][0] == ' '
+    
+    def test_placement_automatique_navire(self):
+        jeu = BatailleNavale()
+        grille = [[' ' for _ in range(10)] for _ in range(10)]
+        
+        # Placer un navire automatiquement
+        resultat = jeu.placer_navire(grille, 3, auto=True)
+        
+        assert resultat == True
+        # Compter le nombre de 'N' dans la grille
+        compte_navires = sum(ligne.count('N') for ligne in grille)
+        assert compte_navires == 3
+    
+    def test_placement_multiple_navires(self):
+        jeu = BatailleNavale()
+        grille = [[' ' for _ in range(10)] for _ in range(10)]
+        
+        # Placer plusieurs navires
+        jeu.placer_navire(grille, 3, auto=True)
+        jeu.placer_navire(grille, 2, auto=True)
+        
+        compte_navires = sum(ligne.count('N') for ligne in grille)
+        assert compte_navires == 5
